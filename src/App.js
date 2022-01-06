cdc4376cf1e8c64fdb890828d0928e5a1f650b00
qiwwwwwww <elfishing07@gmail.com>
2022-01-06 15:20:10 +0800

    add relay learning

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import fetchGraphQL from './fetchGraphQL';
+import graphql from 'babel-plugin-relay/macro';
+import {
+  RelayEnvironmentProvider,
+  loadQuery,
+  usePreloadedQuery,
+} from 'react-relay/hooks';
 
-function App() {
+import RelayEnvironment from './RelayEnvironment';
+
+
+const {Suspense} = React;
+
+// Define a query
+const RepositoryNameQuery = graphql`
+  query AppRepositoryNameQuery {
+    repository(owner: "facebook" name: "relay") {
+      name,
+      id
+    }
+  }
+  `;
+
+  const preloadedQuery = loadQuery(RelayEnvironment, RepositoryNameQuery, {
+
+  });
+
+
+  function App(props) {
+    const data = usePreloadedQuery(RepositoryNameQuery, props.preloadedQuery);
+
+
+  // const [name, setName] = useState(null);
+  // const [id, setId] = useState(null);
+  //
+  // useEffect(() => {
+  //   // let isMount = true;
+  //   fetchGraphQL().then(response => {
+  //       const data = response.data;
+  //       setName(data.repository.name);
+  //       setId(data.repository.id);
+  //
+  //     }).catch(error => {
+  //       console.log(error);
+  //     });
+  //   return () => {};
+  // }, [fetchGraphQL]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        {data.repository.name != null ? `Repository: ${data.repository.name} and id: ${data.repository.id}` : "Loading"}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
 
-export default App;
+
+
+  function AppRoot(props) {
+    return (
+      <RelayEnvironmentProvider environment = {RelayEnvironment}>
+        <Suspense fallback={'Loading....'}>
+          <App preloadedQuery={preloadedQuery}/>
+        </Suspense>
+      </RelayEnvironmentProvider>
+    );
+  }
+
+export default AppRoot;
